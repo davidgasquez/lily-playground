73b51c3b33686d9190aea021e904b75b2e232ced
David Gasquez <davidgasquez@gmail.com>
2022-05-25 18:10:50 +0000

    :art:

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 # Get chain snapshot
-curl -sI https://fil-chain-snapshots-fallback.s3.amazonaws.com/mainnet/minimal_finality_stateroots_latest.car | perl -ne '/x-amz-website-redirect-location:\s(.+)\.car/ && print "$1.sha256sum\n$1.car"' | xargs wget
+curl -sI https://fil-chain-snapshots-fallback.s3.amazonaws.com/mainnet/minimal_finality_stateroots_latest.car |
+    perl -ne '/x-amz-website-redirect-location:\s(.+)\.car/ && print "$1.sha256sum\n$1.car"' |
+    xargs wget
 
 # Initialize Lily
-lily init --repo=/worspaces/lily-playground/.lily --config=/worspaces/lily-playground/config.toml --import-snapshot minimal.car
+lily init \
+    --repo=/worspaces/lily-playground/.lily \
+    --config=/worspaces/lily-playground/config.toml \
+    --import-snapshot minimal.car
