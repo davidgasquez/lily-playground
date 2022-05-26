d7b54b88f63321b420fb7faac0b469fd49b8e145
David Gasquez <davidgasquez@gmail.com>
2022-05-26 10:19:56 +0000

    🚀 Add more commands

--- setup.sh
+++ setup.sh
@@ -6,7 +6,16 @@ curl -sI https://fil-chain-snapshots-fallback.s3.amazonaws.com/mainnet/minimal_f
     xargs wget
 
 # Initialize Lily
-lily init \
-    --repo=/worspaces/lily-playground/.lily \
-    --config=/worspaces/lily-playground/config.toml \
-    --import-snapshot minimal.car
+lily init --repo=.lily --config=config.toml  --import-snapshot minimal.car
+
+# Spawn daemon
+lily daemon
+
+# Wait for sync
+lily sync wait
+
+# Run a walk job
+lily job run --storage=CSV --window=30s --tasks="blocks" walk --from=1841000 --to=1841040
+
+# Run a watch job
+lily job run --storage=CSV --window=30s --tasks="blocks" watch  --confidence=100
