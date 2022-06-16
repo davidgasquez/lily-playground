3dcc03818f02d702df235f7b78a398f67387292f
David Gasquez <davidgasquez@gmail.com>
2022-06-16 09:17:26 +0000

    Use Docker Compose

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 # Get chain snapshot
-aria2c -x16 https://fil-chain-snapshots-fallback.s3.amazonaws.com/mainnet/minimal_finality_stateroots_latest.car |
-    perl -ne '/x-amz-website-redirect-location:\s(.+)\.car/ && print "$1.sha256sum\n$1.car"' |
-    xargs wget
+aria2c -x16 https://fil-chain-snapshots-fallback.s3.amazonaws.com/mainnet/minimal_finality_stateroots_latest.car
 
 # Initialize Lily
 lily init --repo=.lily --config=config.toml --import-snapshot minimal.car
