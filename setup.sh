357eb073a57ab54108d44762f0b65b58c4b420da
David Gasquez <davidgasquez@gmail.com>
2022-06-22 12:58:51 +0000

    :memo:

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 aria2c -x16 https://fil-chain-snapshots-fallback.s3.amazonaws.com/mainnet/minimal_finality_stateroots_latest.car
 
 # Initialize Lily
-lily init --repo=.lily --config=config.toml --import-snapshot minimal.car
+lily init --repo=.lily --config=config.toml --import-snapshot minimal_finality_stateroots_latest.car
 
 # Spawn daemon
 lily daemon --repo=.lily --config=config.toml
@@ -13,7 +13,7 @@ lily daemon --repo=.lily --config=config.toml
 lily sync wait
 
 # Run a walk job
-lily job run --storage=CSV --window=30s --tasks="blocks" walk --from=1841000 --to=1841040
+lily job run --storage=CSV --window=30s --tasks="blocks" walk --from=1841000 --to=1841040 notify --queue="Notifier1"
 
 # Run a watch job
-lily job run --storage=CSV --window=30s --tasks="blocks" watch  --confidence=100
+lily job run --storage=CSV --window=30s --tasks="blocks" watch --confidence=100
