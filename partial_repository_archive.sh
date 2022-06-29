28c6abae4db6b3c577afecd1f99a624c7266a0bb
David Gasquez <davidgasquez@gmail.com>
2022-06-29 08:36:44 +0000

    🚧 Update snapshot

--- partial_repository_archive.sh
+++ partial_repository_archive.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 #
-# Example: ./partial_repository_archive.sh 2022-05-13 2022-06-01
+# Example: ./partial_repository_archive.sh 2022-05-12 2022-06-01
 
 # Set the date range to archive.
 FROM_DATE=$1
 TO_DATE=$2
 
 # Get the related epochs
-FROM_EPOCH=$(lily-shed convert -s date $1_00-00-00)
+FROM_EPOCH=$(lily-shed convert -s date $1_02-00-00)
 TO_EPOCH=$(lily-shed convert -s date $2_00-00-00)
 
 echo "Creating partial repository archive from $FROM_EPOCH to $TO_EPOCH"
 
 # Download chain snapshot
-SNAPSHOT_URL="https://fil-chain-snapshots-fallback.s3.amazonaws.com/mainnet/minimal_finality_stateroots_${FROM_EPOCH}_${FROM_DATE}_00-00-00.car"
+SNAPSHOT_URL="https://fil-chain-snapshots-fallback.s3.amazonaws.com/mainnet/minimal_finality_stateroots_${FROM_EPOCH}_${FROM_DATE}_02-00-00.car"
 echo "Downloading $SNAPSHOT_URL"
 aria2c -x16 $SNAPSHOT_URL
 
